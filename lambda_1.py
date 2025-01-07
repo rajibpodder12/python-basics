64dca73301988a5d7a7459135b44953ce4e3833f
Rajib Podder <rajibpodder12@gmail.com>
2025-01-08 07:07:18 +1100

    lambda expresssion

---
+++ lambda_1.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+
+
+for i in map(lambda x,y: x * y,(10,20),(30,40)):
+    print(i)
---
+++ python-module/collection01.py
@@ -0,0 +1,10 @@
+from collections import Counter
+mylist = [1,1,1,1,1,2,2,2,2,2,3,3,3,3]
+
+print(Counter(mylist))
+
+mylist = ['a','a',10,10,10]
+
+print(Counter(mylist))
+
+
